d7406b769b2fca2f5a3894045bbc20ff2e4303a0
wardpeet <ward@coding-tech.com>
2025-06-18 16:53:38 +0200

    fix watch

--- supabase/functions/api/index.ts
+++ supabase/functions/api/index.ts
@@ -1,13 +1,15 @@
 import { Hono } from "jsr:@hono/hono";
 import { cors } from "jsr:@hono/hono/cors";
+import { HTTPException } from "jsr:@hono/hono/http-exception";
+import { stream } from "jsr:@hono/hono/streaming";
 import {
   getWorkflowByIdHandler,
   getWorkflowRunsHandler,
   createWorkflowRunHandler,
   startWorkflowRunHandler,
-  watchWorkflowHandler,
 } from "@mastra/server/handlers/workflows";
 import { mastra } from "./src/mastra/index.ts";
+import { WorkflowRunState } from "@mastra/core/workflows";
 // You can set the basePath with Hono
 const functionName = "api";
 const app = new Hono().basePath(`/${functionName}`);
@@ -60,15 +62,17 @@ app.post("workflows/:id/start", async (c) => {
   const id = c.req.param("id");
   const { inputData } = await c.req.json();
   const runId = c.req.query("runId");
+  console.log({ runId, inputData });
   try {
-    return c.json(
-      await startWorkflowRunHandler({
-        mastra,
-        workflowId: id,
-        inputData,
-        runId,
-      })
-    );
+    const result = await startWorkflowRunHandler({
+      mastra,
+      workflowId: id,
+      inputData,
+      runId,
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    return c.json(result);
   } catch (error) {
     console.log(error);
     return c.json(
@@ -79,18 +83,103 @@ app.post("workflows/:id/start", async (c) => {
     );
   }
 });
-app.get("workflows/:id/watch", async (c) => {
-  const id = c.req.param("id");
-  const runId = c.req.query("runId");
+
+app.get("workflows/:id/watch", (c) => {
   try {
-    return c.json(
-      await watchWorkflowHandler({
-        mastra,
-        workflowId: id,
-        runId,
-      })
+    console.log("watch workflow");
+    const logger = mastra.getLogger();
+    const id = c.req.param("id");
+    const runId = c.req.query("runId");
+
+    if (!runId) {
+      throw new HTTPException(400, {
+        message: "runId required to watch workflow",
+      });
+    }
+
+    const seenSnapshots = new Set<number>();
+    return stream(
+      c,
+      async (stream) => {
+        try {
+          const storage = mastra.getStorage();
+          if (!storage) {
+            throw new HTTPException(500, {
+              message: "Storage not found",
+            });
+          }
+
+          let i = 0;
+          let snapshot: WorkflowRunState | null = null;
+          // Polling datatabse for status, need to implement some kind of pubsub to make it work in Deno,
+          // Works for now, just not perfect
+          while (
+            snapshot === null ||
+            (snapshot.status !== "success" && snapshot.status !== "failed")
+          ) {
+            console.log("load snapshot", { id, runId, i });
+
+            snapshot = await storage!.loadWorkflowSnapshot({
+              workflowName: id,
+              runId,
+            });
+
+            if (!snapshot) {
+              throw new HTTPException(500, {
+                message: "Snapshot not found",
+              });
+            }
+
+            const chunk = {
+              type: "watch",
+              payload: {
+                // not supported now...
+                // currentStep: {
+                //   id: "test-step",
+                //   status: "running",
+                //   payload: {
+                //     city: "Ibadan",
+                //   },
+                //   startedAt: 1750253045265,
+                // },
+                result: snapshot.result,
+                error: snapshot.error,
+                workflowState: {
+                  status: snapshot.status,
+                  steps: snapshot.context,
+                  result: snapshot.result,
+                  error: snapshot.error,
+                },
+              },
+              eventTimestamp: snapshot.timestamp,
+              runId,
+            };
+
+            // console.log("chunk", chunk);
+            if (!seenSnapshots.has(chunk.eventTimestamp)) {
+              await stream.write(JSON.stringify(chunk) + "\x1E");
+              seenSnapshots.add(chunk.eventTimestamp);
+            }
+            await new Promise((resolve) => setTimeout(resolve, 300));
+          }
+          console.log("done");
+        } catch (err) {
+          console.log("error????");
+          mastra
+            .getLogger()
+            .error(
+              "Error in watch stream: " +
+                ((err as Error)?.message ?? "Unknown error")
+            );
+        }
+      },
+      async (err) => {
+        console.log("wtf???");
+        logger.error("Error in watch stream: " + err?.message);
+      }
     );
   } catch (error) {
+    console.log("error", error);
     return c.json(
       {
         error: "Failed to watch workflow",
@@ -99,4 +188,5 @@ app.get("workflows/:id/watch", async (c) => {
     );
   }
 });
+
 Deno.serve(app.fetch);
--- supabase/functions/api/src/mastra/workflows/test.ts
+++ supabase/functions/api/src/mastra/workflows/test.ts
@@ -2,7 +2,7 @@ import { createWorkflow, createStep } from "@mastra/core/workflows";
 import { z } from "zod";
 const cityCoordinatesStep = createStep({
   id: "test-step",
-  description: "Gets the city name",
+  description: "Gets the city name and puts it in all caps",
   inputSchema: z.object({
     city: z.string(),
   }),
@@ -11,24 +11,25 @@ const cityCoordinatesStep = createStep({
   }),
   execute: async ({ inputData }) => {
     return {
-      city_name: inputData.city,
+      city_name: inputData.city.toUpperCase(),
     };
   },
 });
 const step2 = createStep({
   id: "test-step2",
-  description: "Gets the city name",
+  description: "Gets the city name and reverses it",
   inputSchema: z.object({
     city_name: z.string(),
   }),
   outputSchema: z.object({
-    city_name: z.string(),
+    name_city: z.string(),
   }),
   execute: async ({ inputData }) => {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("test-step2", inputData);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     return {
-      city_name: inputData.city_name,
+      name_city: inputData.city_name.split("").reverse().join(""),
     };
   },
 });
@@ -40,7 +41,7 @@ export const dummyWorkflow = createWorkflow({
     city: z.string(),
   }),
   outputSchema: z.object({
-    city_name: z.string(),
+    name_city: z.string(),
   }),
 })
   .then(cityCoordinatesStep)
